07314f522326cc67fbccd01ccbc9b1be9162e889
kwaku <44327202+mr-nansi@users.noreply.github.com>
2020-01-27 18:58:43 +0000

    got cards right

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,34 +7,122 @@ const Projects = () => {
   return (
     <div className="section">
       <div className="container">
-        <div className="card">
-          <div className="card-image">
-            <figure className="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
-            </figure>
-          </div>
-          <div className="card-content">
-            <div className="media">
-              <div className="media-left">
-                <figure className="image is-48x48">
-                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+
+        <div className="columns">
+          <div className="column">
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
                 </figure>
               </div>
-              <div className="media-content">
-                <p className="title is-4">John Smith</p>
-                <p className="subtitle is-6">@johnsmith</p>
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">Invasion</p>
+                    <p className="subtitle is-6">Project-1</p>
+                  </div>
+                </div>
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+              <a href="#">#css</a> <a href="#">#responsive</a>
+                </div>
               </div>
             </div>
+          </div>
 
-            <div className="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          <div className="column">
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">Park-My-Bike</p>
+                    <p className="subtitle is-6">Project-2</p>
+                  </div>
+                </div>
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
               <a href="#">#css</a> <a href="#">#responsive</a>
-              <br />
-              <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
             </div>
           </div>
+
+          <div className="column">
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">the-kitchen</p>
+                    <p className="subtitle is-6">Project-3</p>
+                  </div>
+                </div>
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+              <a href="#">#css</a> <a href="#">#responsive</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">TripBit</p>
+                    <p className="subtitle is-6">Project-4</p>
+                  </div>
+                </div>
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+              <a href="#">#css</a> <a href="#">#responsive</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
+
       </div>
     </div>
   )
