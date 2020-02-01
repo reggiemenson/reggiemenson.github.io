e65f8287ae5fa8cf1be8caef05d014721ce3dfa8
kwaku <44327202+mr-nansi@users.noreply.github.com>
2020-02-01 09:48:29 +0000

    svg additions

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Image = () => {
-  return (<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+  return (<svg className="main-logo" version="1.0" xmlns="http://www.w3.org/2000/svg"
       width="100%" height="100%" viewBox="0 0 726.000000 1280.000000"
       preserveAspectRatio="xMidYMid meet">
       <metadata>
